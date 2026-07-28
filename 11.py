cccd3051d6e8ea13b286ca11515d48736f92d98c
liuzhe250 <qawsedrfzxc123@qq.com>
2026-07-28 12:53:43 +0800

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,74 @@
+==== Epoch 10 | Train Loss:24767.4629 | Test Loss:11177.5859 ====
+density    | MAE=1.3193, RMSE=1.3258, R2=-96.8898
+H50        | MAE=39.3233, RMSE=41.6596, R2=-8.1976
+deltaHf    | MAE=172.8295, RMSE=178.3129, R2=-15.5082
+
+==== Epoch 20 | Train Loss:24439.6821 | Test Loss:10787.6113 ====
+density    | MAE=0.1024, RMSE=0.1382, R2=-0.0637
+H50        | MAE=36.4495, RMSE=38.9708, R2=-7.0487
+deltaHf    | MAE=170.0591, RMSE=175.6248, R2=-15.0142
+
+==== Epoch 30 | Train Loss:23874.3140 | Test Loss:10083.5879 ====
+density    | MAE=0.1993, RMSE=0.2369, R2=-2.1256
+H50        | MAE=30.5846, RMSE=33.5783, R2=-4.9753
+deltaHf    | MAE=164.9297, RMSE=170.6552, R2=-14.1207
+
+==== Epoch 40 | Train Loss:22850.6152 | Test Loss:8853.2275 ====
+density    | MAE=0.1111, RMSE=0.1294, R2=0.0679
+H50        | MAE=19.5255, RMSE=24.0197, R2=-2.0576
+deltaHf    | MAE=155.1329, RMSE=161.1916, R2=-12.4902
+
+==== Epoch 50 | Train Loss:21535.3818 | Test Loss:7234.3535 ====
+density    | MAE=0.1503, RMSE=0.1639, R2=-0.4958
+H50        | MAE=12.4882, RMSE=14.4281, R2=-0.1032
+deltaHf    | MAE=139.9493, RMSE=146.6113, R2=-10.1601
+
+==== Epoch 60 | Train Loss:20303.7759 | Test Loss:5599.4204 ====
+density    | MAE=0.1708, RMSE=0.2080, R2=-1.4086
+H50        | MAE=19.0546, RMSE=21.8549, R2=-1.5313
+deltaHf    | MAE=120.0876, RMSE=127.7520, R2=-7.4736
+
+==== Epoch 70 | Train Loss:19502.7793 | Test Loss:4207.7207 ====
+density    | MAE=0.1517, RMSE=0.1667, R2=-0.5470
+H50        | MAE=26.9453, RMSE=30.6121, R2=-3.9663
+deltaHf    | MAE=98.9768, RMSE=108.1020, R2=-5.0674
+
+==== Epoch 80 | Train Loss:18984.4756 | Test Loss:3040.5029 ====
+density    | MAE=0.2310, RMSE=0.2645, R2=-2.8957
+H50        | MAE=25.5562, RMSE=29.3878, R2=-3.5770
+deltaHf    | MAE=79.8563, RMSE=90.8724, R2=-3.2874
+
+==== Epoch 90 | Train Loss:18654.3594 | Test Loss:2299.8621 ====
+density    | MAE=0.1425, RMSE=0.1505, R2=-0.2608
+H50        | MAE=20.2000, RMSE=24.4851, R2=-2.1772
+deltaHf    | MAE=66.5234, RMSE=79.3728, R2=-2.2710
+
+==== Epoch 100 | Train Loss:18546.8755 | Test Loss:1930.4514 ====
+density    | MAE=0.1663, RMSE=0.1978, R2=-1.1794
+H50        | MAE=19.1585, RMSE=22.0930, R2=-1.5868
+deltaHf    | MAE=58.5845, RMSE=72.8232, R2=-1.7534
+
+==== Epoch 110 | Train Loss:18532.2944 | Test Loss:1752.8909 ====
+density    | MAE=0.1424, RMSE=0.1503, R2=-0.2582
+H50        | MAE=18.9224, RMSE=21.5730, R2=-1.4664
+deltaHf    | MAE=54.0761, RMSE=69.2333, R2=-1.4887
+
+==== Epoch 120 | Train Loss:18533.0806 | Test Loss:1656.9825 ====
+density    | MAE=0.1041, RMSE=0.1392, R2=-0.0791
+H50        | MAE=18.9452, RMSE=21.6227, R2=-1.4778
+deltaHf    | MAE=51.3382, RMSE=67.1073, R2=-1.3382
+
+==== Epoch 130 | Train Loss:18510.1152 | Test Loss:1588.5813 ====
+density    | MAE=0.2501, RMSE=0.2813, R2=-3.4065
+H50        | MAE=18.9020, RMSE=21.5282, R2=-1.4562
+deltaHf    | MAE=49.3491, RMSE=65.5912, R2=-1.2337
+
+==== Epoch 140 | Train Loss:18509.7256 | Test Loss:1584.5941 ====
+density    | MAE=0.1545, RMSE=0.1722, R2=-0.6517
+H50        | MAE=18.7761, RMSE=21.2548, R2=-1.3942
+deltaHf    | MAE=49.3472, RMSE=65.5895, R2=-1.2336
+
+==== Epoch 150 | Train Loss:18509.7847 | Test Loss:1550.0929 ====
+density    | MAE=0.1522, RMSE=0.1677, R2=-0.5665
+H50        | MAE=18.8573, RMSE=21.4304, R2=-1.4339
+deltaHf    | MAE=48.2149, RMSE=64.7378, R2=-1.1760
